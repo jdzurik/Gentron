427b628e657aae152cbb6e7285a7db59283bb111
Tom Foley <8699633+tom-foley@users.noreply.github.com>
2018-11-13 23:03:21 -0500

    Separated query providers out from database sources so that they can be injected into app -- provider module injection fixes issue with wepback not being able load mssql/msnodesqlv8 modules (#13)

---
+++ Gentron.Library/queryProviders/IQueryProvider.ts
@@ -0,0 +1,5 @@
+﻿import { Result, TDataSourceResult } from "../results";
+
+export default interface IQueryProvider {
+    executeQuery(connStr: string, queryStr: string, formatResults: boolean): Promise<Result<TDataSourceResult>>;
+}
